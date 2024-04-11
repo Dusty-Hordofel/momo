1b0283133dfaf3504be5e8dc450017b59ff84751
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2024-04-11 18:06:31 +0200

    Create AddCenterButton.tsx

---
+++ src/app/(private)/dashboard/user/AddCenterButton.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const AddCenterButton = () => {
+  return (
+    <Select>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Theme" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="light">Light</SelectItem>
+        <SelectItem value="dark">Dark</SelectItem>
+        <SelectItem value="system">System</SelectItem>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default AddCenterButton;
