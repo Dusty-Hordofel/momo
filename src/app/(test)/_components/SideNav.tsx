098eb5e6a611970a29861921b7389a7ebc2100d6
Dusty-Hordofel <104115392+Dusty-Hordofel@users.noreply.github.com>
2024-04-11 17:40:17 +0200

    delete MoreDropDown

--- src/app/(test)/_components/SideNav.tsx
+++ src/app/(test)/_components/SideNav.tsx
@@ -1,14 +1,9 @@
-// import { instagramLinks } from "@/assets/icons/others/links";
 import Logo from "./Logo";
 import NavLinks from "./NavLinks";
-// import DropdownMenu from "./BurgerMenu";
 import ThreadsLink from "./ThreadsLink";
 import { instagramLinks } from "@/assets/icons/others/links";
-// import MoreDropDown from "./MoreDropdown";
 
 const SideNav = () => {
-  // console.log("🚀 ~ instagramLinks: SideNav", instagramLinks);
-
   return (
     <div className="flex h-full flex-col py-4 items-center bg-black justify-between">
       <div className="bg-white border-t dark:bg-black  w-full">
@@ -16,10 +11,7 @@ const SideNav = () => {
         <NavLinks instagramLinks={instagramLinks} />
       </div>
       <div className="bg-white w-full">
-        {/* <MoreDropDown /> */}
         <ThreadsLink />
-        {/* <DropdownMenu /> */}
-        {/* <MoreDropDown /> */}
       </div>
     </div>
   );
